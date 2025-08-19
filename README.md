52a2c19a59b97ae709ac8d04c562f1c65ddbc21e
Cem Dervis <cem@dervis.de>
2025-08-19 11:25:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<div class="title-block" style="text-align: center;" align="center">
+
+<p><img title="Go C++ logo" src="Resources/gocpp.png" width="300"></p>
+
+## Your quickstart to C++ projects
+
+**[gocpp.dev]**
+
+[gocpp.dev]: https://gocpp.dev
+
+</div>
+
+---
+
+With a single command, Go C++ sets up:
+
+- A CMake-based C++ project
+- Package management using [CPM](https://github.com/cpm-cmake/CPM.cmake)
+- Formatting using clang-format
+- Compile-time checks using [Cppcheck](https://cppcheck.sourceforge.io/) and clang-tidy
+- Code-hardening flags enabled, as recommended by the [OpenSSF](https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++.html)
+- Flags that enable runtime memory error detection using AddressSanitizer
+
+The generated project is cross-platform and uses a modern C++ standard.
+
+Please see the [homepage](https://gocpp.dev) for a quick overview.
---
+++ Template/cmake/CompilerSetup.cmake
@@ -0,0 +1,139 @@
+#
+# Helper utilities for setting up a target with sensible compiler flags.
+# 
+# Reference used for some compiler flags:
+# https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++.html
+#
+#
+
+#
+# Options
+#
+option(ENABLE_CPPCHECK "Enable additional checks using cppcheck?" OFF)
+option(ENABLE_HARDENING "Enable C++ compiler hardening flags?" OFF)
+option(ENABLE_ADDRESS_SANITIZER "Enable AddressSanitizer?" OFF)
+
+
+#
+# Enable AddressSanitizer
+#
+if (ENABLE_ADDRESS_SANITIZER)
+  message(STATUS "Enabling AddressSanitizer globally")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -fno-omit-frame-pointer")
+endif()
+
+
+#
+# Enables common warnings, treats warnings as errors, ...
+#
+function(enable_sane_compiler_flags targetName)
+  message(STATUS "Enabling sane compiler flags for ${targetName}")
+
+  if (MSVC)
+    target_compile_options(${targetName} PRIVATE
+      /W4 # Warning level 4
+      /WX # Treat warnings as errors
+    )
+  else()
+    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+      set(IS_GCC TRUE)
+    elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+      set(IS_CLANG TRUE)
+    endif()
+
+    if (IS_GCC OR IS_CLANG)
+      target_compile_options(${targetName} PRIVATE
+        -Wall
+        -Wextra
+        -Wpedantic
+        -pedantic-errors
+        -Werror
+        -Wimplicit-fallthrough
+        -Wformat
+        -Werror=format-security
+      )
+    endif()
+  endif()
+endfunction()
+
+
+#
+# Enables C++ hardening flags for a target.
+#
+function (enable_hardening_compiler_flags targetName)
+  message(STATUS "Enabling hardening flags for ${targetName}")
+
+  if (MSVC)
+    target_compile_definitions(${targetName} PRIVATE
+      -D_ITERATOR_DEBUG_LEVEL=2 # Enable checked iterators in the STL
+    )
+  else()
+    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+      set(IS_GCC TRUE)
+    elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+      set(IS_CLANG TRUE)
+    endif()
+
+    if (IS_GCC OR IS_CLANG)
+      # AddressSanitizer misbehaves when FORTIFY_SOURCE is enabled.
+      # Only fortify source when we're not going to use AddressSanitizer anyway.
+      if (NOT ENABLE_ADDRESS_SANITIZER)
+        target_compile_definitions(${targetName} PRIVATE
+          -D_FORTIFY_SOURCE=3
+          -D_GLIBCXX_ASSERTIONS
+        )
+      endif()
+
+      #target_compile_options(${targetName} PRIVATE
+      #  -fstack-clash-protection
+      #  -fstack-protector-strong
+      #  -fcf-protection=full
+      #)
+
+      target_compile_options(${targetName} PRIVATE
+        -fno-delete-null-pointer-checks
+        -fno-strict-overflow
+        -fno-strict-aliasing
+      )
+    endif()
+  endif()
+endfunction()
+
+
+
+#
+# Enables cppcheck checks for a target.
+#
+function (enable_cppcheck targetName)
+  message(STATUS "Enabling cppcheck checks for ${targetName}")
+  find_program(CPPCHECK_EXE NAMES "cppcheck")
+
+  if (CPPCHECK_EXE)
+    set(CPPCHECK_CMD ${CPPCHECK_EXE})
+    list(APPEND CPPCHECK_CMD
+      "--enable=warning"
+      "--inconclusive"
+      "--force"
+      "--inline-suppr"
+    )
+    set_target_properties(${targetName} PROPERTIES CXX_CPPCHECK "${CPPCHECK_CMD}")
+  else()
+    message(WARNING "cppcheck was not found; if you want cppcheck checks, please install it first")
+  endif()
+endfunction()
+
+
+#
+# Enables all configured flags and checks for a target.
+#
+function(setup_compiler_for targetName)
+  enable_sane_compiler_flags(${targetName})
+
+  if (ENABLE_HARDENING)
+    enable_hardening_compiler_flags(${targetName})
+  endif()
+
+  if (ENABLE_CPPCHECK)
+    enable_cppcheck(${targetName})
+  endif()
+endfunction()
